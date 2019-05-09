71ebda3dc369aed2c57b11375e51fee923577939
vjeferson <valdeci_jeferson@hotmail.com>
2019-05-09 20:30:56 -0300

    Criação das Rotas, Models e Controllers com CRUD básico

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require('require-dir');
 
+//Inicia o app
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Inicia o Bd
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
